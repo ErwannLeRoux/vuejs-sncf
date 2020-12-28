052b0f659ff8311ab76e3bc3a80342a2cb27f4ae
Antoine Gonzalez <antoine.gonzalez61@live.fr>
2020-12-28 13:51:53 +0100

    Instanciate Vue-router #5.1

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+    <h2>Dashboard Page</h2>
+</template>
+
+<script>
+export default {
+    name: "Dashboard",
+    components: {
+
+    },
+    data() {
+        return {
+        };
+    },
+    methods :{
+
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
+
