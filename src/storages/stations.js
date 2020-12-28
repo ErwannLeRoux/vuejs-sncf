dcdf2642f0f904e5a3a0ed611096122c105015ff
Erwann <erwann.leroux1@gmail.com>
2020-12-28 21:25:00 +0100

    handicap facilities fetch

--- src/storages/stations.js
+++ src/storages/stations.js
@@ -5,26 +5,31 @@ export const store = createStore({
     state() {
         return {
             stations: [],
-            cleanliness: []
+            cleanliness: [],
+            handicapFacilities: []
         }
     },
     getters: {
         getStations: (state) => {
-            return state.stations;
+            return state.stations
         },
         getCleanliness: (state) => {
-            return state.cleanliness;
+            return state.cleanliness
+        },
+        getHandicapFacilities: (state) => {
+            return state.handicapFacilities
         },
     },
     mutations: {
         disconnect(state){
-            state.stations = [];
+            state.stations = []
             state.cleanliness = []
+            state.handicapFacilities = []
         },
         getStations(state) {
             axios.get("https://data.sncf.com/api/records/1.0/search/?dataset=liste-des-gares&q=&rows=-1")
             .then((response) => {
-                state.stations = response.data.records;
+                state.stations = response.data.records
             }).catch((error) => {
                 console.log(error)
             });
@@ -32,7 +37,15 @@ export const store = createStore({
         getCleanliness(state) {
             axios.get("https://data.sncf.com/api/records/1.0/search/?dataset=proprete-en-gare&q=&rows=-1")
                 .then((response) => {
-                    state.cleanliness = response.data.records;
+                    state.cleanliness = response.data.records
+                }).catch((error) => {
+                console.log(error)
+            });
+        },
+        getHandicapFacilities(state) {
+            axios.get("https://ressources.data.sncf.com/api/records/1.0/search/?dataset=accompagnement-pmr-gares&rows=-1")
+                .then((response) => {
+                    state.handicapFacilities = response.data.records
                 }).catch((error) => {
                 console.log(error)
             });
