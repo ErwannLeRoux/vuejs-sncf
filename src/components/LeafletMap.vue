c2be53b352acd02406520984987c8c899cc5d88b
Erwann <erwann.leroux1@gmail.com>
2021-01-03 21:07:47 +0100

    Markers colors and refactoring

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -39,31 +39,10 @@
         },
         methods: {
             regionChanged: function() {
-                this.markers.clearLayers()
-                this.map.remove()
-
-                store.dispatch("getStations", {region_name: this.regionSelected}).then((res) => {
+                store.dispatch("getStations", {region_name: this.regionSelected}).then((stations) => {
                     store.dispatch("getRegions", {region_name: this.regionSelected}).then((region) => {
-
-                        this.map = L.map('mapContainer').setView([region.lat, region.lng], 7);
-
-                        this.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                            attribution: '',
-                        });
-                        this.tileLayer.addTo(this.map);
-                        this.map.setView([region.lat, region.lng], 7);
-
-                        res.forEach((station) => {
-                            let lat = station.wgs_84[0]
-                            let lng = station.wgs_84[1]
-                            if(lat && lng) {
-                                let marker = L.marker([station.wgs_84[0], station.wgs_84[1]])
-                                marker.bindTooltip(station.name).openTooltip();
-                                this.markers.addLayer(marker);
-                            }
-                        })
-
-                        this.markers.addTo(this.map)
+                        let viewBox = { lat: region.lat, lng: region.lng }
+                        this.buildMap(stations, viewBox, 7, false)
                     }).catch(console.error)
                 }).catch(console.error)
             },
@@ -71,34 +50,68 @@
                 delete L.Icon.Default.prototype._getIconUrl;
 
                 L.Icon.Default.mergeOptions({
-                    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-                    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-                    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+                    iconRetinaUrl: require('../assets/images/marker-green/marker-icon-2x.png'),
+                    iconUrl: require('../assets/images/marker-green/marker-icon.png'),
+                    shadowUrl: require('../assets/images/marker-shadow.png'),
                 });
             },
+            buildMap(stations, viewbox, zoom, init) {
+                if(!init) {
+                    // cleaning the map
+                    this.markers.clearLayers()
+                    this.map.remove()
+                }
+                this.markers = L.layerGroup()
+                this.map = L.map('mapContainer').setView([viewbox.lat, viewbox.lng], zoom);
+                this.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '',
+                }).addTo(this.map)
+
+                stations.forEach((station) => {
+                    let scores = station.scores_for_years
+                    let color = 'unknown'
+                    if(scores.length > 0) {
+                        let currentYear = scores.find(y => y.year == '2020')
+                        if(currentYear) {
+                            let avg = currentYear.average_score
+                            if(avg >= 95) {
+                                color = 'green'
+                            } else if (avg >= 90)  {
+                                color = 'orange'
+                            } else {
+                                color = 'red'
+                            }
+
+                        }
+                    }
+                    let icon = new L.Icon({
+                        iconRetinaUrl: require(`../assets/images/marker-${color}/marker-icon-2x.png`),
+                        iconUrl: require(`../assets/images/marker-${color}/marker-icon.png`),
+                        shadowUrl: require(`../assets/images/marker-shadow.png`),
+                        iconSize: [25, 41], // size of the icon
+                        iconAnchor: [0, 21], // point of the icon which will correspond to marker's location
+                        popupAnchor: [0, -51] // point from which the popup should open relative to the iconAnchor
+                    })
+
+                    if(station.wgs_84[0] && station.wgs_84[1] && station.scores_for_years.length != 0) {
+                        let marker = L.marker([station.wgs_84[0], station.wgs_84[1]], {
+                            icon: icon
+                        })
+
+                        marker.bindTooltip(station.name).openTooltip();
+                        this.markers.addLayer(marker);
+                    }
+                })
+                this.markers.addTo(this.map)
+            }
         },
         mounted() {
             this.initMapContext()
             store.commit("getRegions")
-
-            this.markers = L.layerGroup()
-
-            store.dispatch("getStations", {region_name: this.regionSelected}).then((res) => {
+            store.dispatch("getStations", {region_name: this.regionSelected}).then((stations) => {
               store.dispatch("getRegions", {region_name: this.regionSelected}).then((region) => {
-
-                this.map = L.map('mapContainer').setView([region.lat, region.lng], 7);
-
-                this.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                      attribution: '',
-                  });
-                this.tileLayer.addTo(this.map);
-
-                res.forEach((station) => {
-                    let marker = L.marker([station.wgs_84[0], station.wgs_84[1]])
-                    marker.bindTooltip(station.name).openTooltip();
-                    this.markers.addLayer(marker);
-                })
-                this.markers.addTo(this.map)
+                  let viewBox = { lat: region.lat, lng: region.lng }
+                  this.buildMap(stations, viewBox, 7, true)
               })
             })
         }
