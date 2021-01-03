b163f95463fd12c2b11c016399e3ab24d108838c
Erwann <erwann.leroux1@gmail.com>
2021-01-03 18:26:11 +0100

    patch for null map

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,12 +1,13 @@
 <template>
     <div id="mapContainer" ></div>
     <select v-model="regionSelected" @change="regionChanged()">
-      <option v-for="option in options" v-bind:key="option" v-bind:value="option">
-        {{ option }}
+      <option v-for="option in options" v-bind:key="option.region_name" v-bind:value="option.region_name">
+        {{ option.region_name }}
       </option>
     </select>
 </template>
 <script>
+    const L = window.L
     import {
 
 
@@ -24,7 +25,6 @@
         data() {
             return {
                 regionSelected: 'Normandie',
-                options: [],
                 markers: [],
                 map: null,
                 zoom: 5,
@@ -33,51 +33,53 @@
             };
         },
         computed: {
-            gares: function() {
-                return store.getters.getStations
-            }
+            options: function () {
+                return store.getters.getRegions
+            },
         },
         methods: {
             regionChanged: function() {
+                this.markers.clearLayers()
+                this.map.remove()
 
-              const L = window.L
+                store.dispatch("getStations", {region_name: this.regionSelected}).then((res) => {
+                    store.dispatch("getRegions", {region_name: this.regionSelected}).then((region) => {
 
-              store.dispatch("getStations", {region_name: this.regionSelected}).then((res) => {
-                store.dispatch("getRegions", {region_name: this.regionSelected}).then((region) => {
-                  this.markers.removeFrom(this.map)
-                  this.markers = L.layerGroup()
-                  this.map.setView([region.lat, region.lng], 7);
+                        this.map = L.map('mapContainer').setView([region.lat, region.lng], 7);
 
-                  this.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                      attribution: '',
-                  });
-                  this.tileLayer.addTo(this.map);
+                        this.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                            attribution: '',
+                        });
+                        this.tileLayer.addTo(this.map);
+                        this.map.setView([region.lat, region.lng], 7);
 
-                  res.forEach((station) => {
-                      let marker = L.marker([station.wgs_84[0], station.wgs_84[1]])
-                      marker.bindTooltip(station.name).openTooltip();
-                      this.markers.addLayer(marker);
-                  })
-                  this.markers.addTo(this.map)
+                        res.forEach((station) => {
+                            let lat = station.wgs_84[0]
+                            let lng = station.wgs_84[1]
+                            if(lat && lng) {
+                                let marker = L.marker([station.wgs_84[0], station.wgs_84[1]])
+                                marker.bindTooltip(station.name).openTooltip();
+                                this.markers.addLayer(marker);
+                            }
+                        })
+
+                        this.markers.addTo(this.map)
+                    }).catch(console.error)
                 }).catch(console.error)
-            }).catch(console.error)
-            }
+            },
+            initMapContext: function () {
+                delete L.Icon.Default.prototype._getIconUrl;
+
+                L.Icon.Default.mergeOptions({
+                    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+                    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+                    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+                });
+            },
         },
         mounted() {
-            store.dispatch("getRegionList").then((response) => {
-              response.forEach((region) => {
-                this.options.push(region.region_name)
-              })
-            })
-
-            const L = window.L
-            delete L.Icon.Default.prototype._getIconUrl;
-
-            L.Icon.Default.mergeOptions({
-                iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-                iconUrl: require('leaflet/dist/images/marker-icon.png'),
-                shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-            });
+            this.initMapContext()
+            store.commit("getRegions")
 
             this.markers = L.layerGroup()
 
@@ -87,8 +89,8 @@
                 this.map = L.map('mapContainer').setView([region.lat, region.lng], 7);
 
                 this.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '',
-                });
+                      attribution: '',
+                  });
                 this.tileLayer.addTo(this.map);
 
                 res.forEach((station) => {
--- src/storages/stations.js
+++ src/storages/stations.js
@@ -4,90 +4,24 @@ import { createStore } from 'vuex'
 export const store = createStore({
     state() {
         return {
-            stations: [],
-            cleanliness: [],
-            handicapFacilities: [],
-            wifiEquipment: [],
-            salesWaitingTime: [],
-            waitingServices: [],
+            regions: [],
         }
     },
     getters: {
-        getStations: (state) => {
-            return state.stations
-        },
-        getCleanliness: (state) => {
-            return state.cleanliness
-        },
-        getHandicapFacilities: (state) => {
-            return state.handicapFacilities
-        },
-        getWifiEquipment: (state) => {
-            return state.wifiEquipment
-        },
-        getSalesWaitingTime: (state) => {
-            return state.salesWaitingTime
-        },
-        getWaitingServices: (state) => {
-            return state.waitingServices
-        },
+        getRegions: (state) => {
+            return state.regions
+        }
     },
     mutations: {
         disconnect(state){
-            state.stations = []
-            state.cleanliness = []
-            state.handicapFacilities = []
-            state.wifiEquipment = []
-            state.salesWaitingTime = []
-            state.waitingServices = []
+            state.regions = []
         },
-        getStations(state) {
-            axios.get("https://data.sncf.com/api/records/1.0/search/?dataset=liste-des-gares&q=&rows=-1")
-            .then((response) => {
-                state.stations = response.data.records
-            }).catch((error) => {
-                console.log(error)
-            });
-        },
-        getCleanliness(state) {
-            axios.get("https://data.sncf.com/api/records/1.0/search/?dataset=proprete-en-gare&q=&rows=-1")
-                .then((response) => {
-                    state.cleanliness = response.data.records
-                }).catch((error) => {
-                console.log(error)
-            });
-        },
-        getHandicapFacilities(state) {
-            axios.get("https://ressources.data.sncf.com/api/records/1.0/search/?dataset=accompagnement-pmr-gares&rows=-1")
-                .then((response) => {
-                    state.handicapFacilities = response.data.records
-                }).catch((error) => {
-                console.log(error)
-            });
-        },
-        getWifiEquipment(state) {
-            axios.get("https://ressources.data.sncf.com/api/records/1.0/search/?dataset=gares-equipees-du-wifi&rows=-1")
-                .then((response) => {
-                    state.wifiEquipment = response.data.records
-                }).catch((error) => {
-                console.log(error)
-            });
-        },
-        getSalesWaitingTime(state) {
-            axios.get("https://ressources.data.sncf.com/api/records/1.0/search/?dataset=attente-au-guichet&rows=-1")
-                .then((response) => {
-                    state.salesWaitingTime = response.data.records
-                }).catch((error) => {
-                console.log(error)
-            });
-        },
-        getWaitingServices(state) {
-            axios.get("https://ressources.data.sncf.com/api/records/1.0/search/?dataset=gares-pianos&q=&rows=-1")
-                .then((response) => {
-                    state.waitingServices = response.data.records
-                }).catch((error) => {
-                console.log(error)
-            });
+        getRegions (state) {
+            axios.get(`http://antoinegonzalez.fr:8080/regions`)
+                .then(result => {
+                    state.regions = result.data.data
+                })
+                .catch(console.error);
         },
     },
     actions: {
@@ -97,7 +31,6 @@ export const store = createStore({
             return new Promise(( resolve, reject ) => {
                 axios.get(`http://antoinegonzalez.fr:8080/stations/?region=${region}`)
                     .then(result => {
-                        context.commit("getStations", result.data.data);
                         resolve(result.data.data);
                     })
                     .catch((err) => reject(err));
